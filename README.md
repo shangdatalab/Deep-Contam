e35016d0702b4d9bcf749b334bfe7d85dd3a9118
Feng Yao <yaof97@gmail.com>
2024-06-20 00:47:15 -0700

    update quick start

--- README.md
+++ README.md
@@ -22,48 +22,39 @@
 Deep Contam represents the cross-lingual contamination that inflates LLMs' benchmark performance while evading existing detection methods. An effective method to detect it is also provided in this repository.
 
 ## Quick Start
-To detect potential hidden contamination in a specific model, specify `model_path` and run the following command.
+To detect potential hidden contamination in a specific model, follow the steps below.
 
-```bash
-python detect.py --model_path MODEL_PATH --dataset_name DATA_NAME
-```
 
-For example,
-```bash
-python detect.py --model_path 'microsoft/phi-2' --dataset_name MMLU,ARC-C,MathQA
-```
+- Install dependencies.
+  ```bash
+  pip install -r requirements.txt
+  ```
+- Specify `model_path` and run the following command.
+  ```bash
+  python detect.py --model_path MODEL_PATH --dataset_name DATA_NAME
+  ```
+  For example,
+  ```bash
+  python detect.py --model_path 'microsoft/phi-2' --dataset_name MMLU,ARC-C,MathQA
+  ```
 
-The output would be:
-```bash
-MMLU
-    original: 23.83
-    generalized: 25.02
-    difference: +1.20
-----------------------
-ARC-C
-    original: 42.92
-    generalized: 47.27
-    difference: +4.35
-----------------------
-MathQA
-    original: 31.32
-    generalized: 38.70
-    difference: +7.38
-```
-
-Environment Setup
-```
-datasets                  2.17.1
-evaluate                  0.4.1
-huggingface-hub           0.23.3
-ipython                   8.22.2
-lm-eval                   0.4.1
-python                    3.10.13
-sentence-transformers     3.0.0
-tokenizers                0.19.1
-tqdm                      4.66.2
-transformers              4.41.2
-```
+  The output would be:
+  ```bash
+  MMLU
+      original: 23.83
+      generalized: 25.02
+      difference: +1.20
+  ----------------------
+  ARC-C
+      original: 42.92
+      generalized: 47.27
+      difference: +4.35
+  ----------------------
+  MathQA
+      original: 31.32
+      generalized: 38.70
+      difference: +7.38
+  ```
 
 ## Data Release
 The generalized versions of the benchmark we constructed to detect the potential contamination are released as follows.
--- detect.py
+++ detect.py
@@ -11,7 +11,7 @@ import yaml
 import argparse
 import lm_eval
 from lm_eval.tasks import include_task_folder
-# include_task_folder("lm-evaluation-harness/lm_eval/tasks/arc")
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Finetune a transformers model on a causal language modeling task")
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 
     model_path = args.model_name_or_path
     config = AutoConfig.from_pretrained(model_path, trust_remote_code=True)
-    tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True, cache_dir="/data/z9sun/.cache", trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True, trust_remote_code=True)
 
     model_args = f"pretrained={model_path}"
     lm = lm_eval.api.registry.get_model("hf").create_from_arg_string(
@@ -73,37 +73,8 @@ if __name__ == "__main__":
             },
         )
     
-    # include_task_folder("/lm-evaluattion-harness/lm_eval/tasks/mmlu_dataset")
-    # lm_eval.tasks.initialize_tasks() # register all tasks from the `lm_eval/tasks` subdirectory. Alternatively, can call `lm_eval.tasks.include_path("path/to/my/custom/task/configs")` to only register a set of tasks in a separate directory.
-    #lm_eval.tasks.include_path("lm-evaluation-harness/lm_eval/tasks/mmlu_dataset")
-    lm_eval.tasks.include_path("/data/z9sun/unmask_llm/unmask_llm/custom_tasks/")
+    lm_eval.tasks.include_path("./custom_tasks/")
     lm_eval.tasks.initialize_tasks()
-
-    # results1 = lm_eval.simple_evaluate( # call simple_evaluate
-    #     model=lm,
-    #     tasks=args.task1.split(","),
-    #     num_fewshot=args.n_shot,
-    #     log_samples=True,
-    #     write_out=True,
-    #     # limit = 3,
-    # )
-
-    # results2 = lm_eval.simple_evaluate( # call simple_evaluate
-    #     model=lm,
-    #     tasks=args.task2.split(","),
-    #     num_fewshot=args.n_shot,
-    #     log_samples=True,
-    #     write_out=True,
-    #     # limit = 3,
-    # )
-    #LLM_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    # print(results)
-
-    # with open('name.txt', 'w', encoding='utf-8') as outfile:
-    #     outfile.write(str(model_path) + '\n') 
-    #     outfile.write('original: ' + str(results1['results'][str(args.task1)]['acc,none']) + '\n')
-    #     outfile.write(str(args.task2) + ': ' + str(results2['results'][str(args.task2)]['acc,none']) + '\n')
-    #     outfile.write('Difference: ' + str(results2['results'][str(args.task2)]['acc,none'] - results1['results'][str(args.task1)]['acc,none']))
 
     dtsets = args.dataset_name.split(",")
     
